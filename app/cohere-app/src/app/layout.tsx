0e63fab23964a4a9b3b695347c729f6a4c5e8c29
Egbewatt Kokou Mwangah Smith <ksupro1@gmail.com>
2024-03-10 16:20:04 -0600

    create helm chart

--- app/cohere-app/src/app/layout.tsx
+++ app/cohere-app/src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Cohere Next App",
+  title: "Cohere App",
   description: "Frontend Assessment",
 }
 
